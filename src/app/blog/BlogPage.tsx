bcb09346daa1d2e0e094a07037dcfd0004d00ad5
Aung Ko Man <aungkoman@gmail.com>
2025-05-20 15:55:35 +0630

    Update BlogPage.tsx

--- src/app/blog/BlogPage.tsx
+++ src/app/blog/BlogPage.tsx
@@ -160,7 +160,7 @@ export default function BlogPage() {
             </div>
           )}
           {!hasMore && (
-            <p className="text-gray-500">You've reached the end of articles</p>
+            <p className="text-gray-500">You&apos;ve reached the end of articles</p>
           )}
         </div>
       </div>
